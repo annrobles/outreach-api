ef25130ba573d7f80f0d12e8816e9fdf1a8de3e5
Florece Ann Robles <fquiles@corebridge.net>
2022-11-08 22:22:09 -0500

    added db migrate and seed

---
+++ database/migrations/2022_10_31_235320_create_company_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('about', 500)->nullable();
+            $table->string('link', 200)->nullable();
+            $table->string('email', 50)->unique();
+            $table->string('contact_number', 15)->nullable();
+            $table->bigInteger('company_skillset_id')->nullable()->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('company', function($table) {
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('company_skillset_id')->references('id')->on('skillset');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company');
+    }
+};
---
+++ database/migrations/2022_10_31_235443_create_student_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('first_name', 100)->nullable();
+            $table->string('last_name', 100)->nullable();
+            $table->string('contact_number', 15)->nullable();
+            $table->string('email', 50)->unique();
+            $table->string('link', 200)->nullable();
+            $table->string('title', 200)->nullable();
+            $table->string('about', 500)->nullable();
+            $table->bigInteger('student_skillset_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('student', function($table) {
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('student_skillset_id')->references('id')->on('skillset');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+};
